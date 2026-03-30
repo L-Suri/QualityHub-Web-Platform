dd2295eb27e80c430344bffed9ea6d0941b55708
Laveena <laveena.suri22@gmail.com>
2026-03-30 13:11:12 -0700

    Story 2.3: Install and configure Material UI with global theme

---
+++ frontend/app/src/App.tsx
@@ -0,0 +1,13 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <Button variant="contained" color="primary">
+        Material UI Working!
+      </Button>
+    </div>
+  );
+}
+
+export default App;
